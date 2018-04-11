e3f572ed667d0af60fd1b83bc09a81c71accc70e
Igor Soroka <stupbird@gmail.com>
2018-04-11 23:16:40 +0200

    error handling added and one more function added to provide message

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+process.env["NTBA_FIX_319"] = 1;
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const transliterator = require('./transliterator.js');
@@ -5,20 +6,20 @@ const transliterator = require('./transliterator.js');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, {polling: true});
-const typingAdvice = "type any text/введи текст";
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  // const chatId = msg.chat.id;
+// the captured "whatever"
+//   const resp = match[1];
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+//   bot.sendMessage(chatId, resp);
+// });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -29,17 +30,23 @@ bot.on('message', (msg) => {
 
 bot.on('inline_query', function (answer) {
     let queryText = answer.query;
-    let transliteratedMessage = queryText === "" ? typingAdvice : transliterator.transliterate(queryText);
+    let transliteratedMessage = createNonEmptyMessage(queryText);
     let newMessageContent = {
-      'message_text' : transliteratedMessage
+        'message_text' : transliteratedMessage
     };
     let queryResult = {
-      'id' : answer.id,
-      'type' : 'article',
-      'title' : transliteratedMessage,
-      'input_message_content' : newMessageContent
+        'id' : answer.id,
+        'type' : 'article',
+        'title' : transliteratedMessage,
+        'input_message_content' : newMessageContent
     };
-
-    bot.answerInlineQuery(answer.id, [queryResult]);
+    bot.answerInlineQuery(answer.id, [queryResult]).catch((error) => {
+        console.error(error.code);
+    });
 });
+
+function createNonEmptyMessage(queryText) {
+    const typingAdvice = "type any text/введи текст";
+    return queryText === "" ? typingAdvice : transliterator.transliterate(queryText);
+}
 
